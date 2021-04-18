7315eb1af99e3e4396b8220b566dfd91d710f00f
Ben Pate <ben@pate.org>
2021-04-17 23:29:53 -0600

    Update transactions.go
    - adding metadata for new echo binding

--- transactions.go
+++ transactions.go
@@ -3,9 +3,9 @@ package steranko
 // SigninTransaction includes all of the information that MUST be posted
 // to Sterenko in order to sign in to the system.
 type SigninTransaction struct {
-	Username      string `json:"username"`      // public username for this person
-	Password      string `json:"password"`      // private (hashed?) password for this person
-	TwoFactorCode string `json:"twoFactorCode"` // [Optional] 2FA code to send to the 2FA plugin
+	Username      string `json:"username"      form:"username"`      // public username for this person
+	Password      string `json:"password"      form:"password"`      // private (hashed?) password for this person
+	TwoFactorCode string `json:"twoFactorCode" form:"twoFactorCode"` // [Optional] 2FA code to send to the 2FA plugin
 }
 
 // SigninResponse includes all the information returned by Steranko
@@ -18,16 +18,16 @@ type SigninResponse struct {
 }
 
 type RequestPasswordResetTransaction struct {
-	Username string `json:"username"` // public username of the person requesting the reset.
+	Username string `json:"username" form:"userame"` // public username of the person requesting the reset.
 }
 
 type RequestPasswordResetResponse struct {
 }
 
 type UpdatePasswordTransaction struct {
-	Username    string `json:"username"`
-	OldPassword string `json:"oldPassword"`
-	NewPassword string `json:"newPassword"`
+	Username    string `json:"username"    form:"username"`
+	OldPassword string `json:"oldPassword" form:"oldPassword"`
+	NewPassword string `json:"newPassword" form:"newPassword"`
 }
 
 type UpdatePasswordResponse struct {
