48e7b4d9d62db652c83d130aee8af3eb0ac123af
Ben Pate <ben@pate.org>
2022-03-14 16:39:40 -0600

    Publish "CreateCertificate" function

--- jwt.go
+++ jwt.go
@@ -1,15 +1,21 @@
 package steranko
 
 import (
+	"net/http"
+
 	"github.com/benpate/derp"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
-// createJWT creates a new JWT token for the provided user.
+// CreateCertificate creates a new JWT token for the provided user.
 // TODO: include additional configuration options when defined.
-func (s *Steranko) createJWT(user User) (string, error) {
+func (s *Steranko) CreateCertificate(ctx echo.Context, user User) error {
 
+	// (short) random sleep to thwart timing attacks
+	sleepRandom(500, 1500)
+
+	// Set up a new JWT token
 	token := jwt.New(jwt.SigningMethodHS256)
 	token.Claims = user.Claims()
 
@@ -17,13 +23,26 @@ func (s *Steranko) createJWT(user User) (string, error) {
 	token.Header["kid"] = keyID
 
 	// Generate encoded token and send it as response.
-	signedString, errr := token.SignedString(key)
+	signedString, err := token.SignedString(key)
 
-	if errr != nil {
-		return "", derp.Wrap(errr, "steranko.PostSigninTransaction", "Error Signing JWT Token")
+	if err != nil {
+		return derp.Wrap(err, "steranko.PostSigninTransaction", "Error Signing JWT Token")
 	}
 
-	return signedString, nil
+	// Set a cookie in the response context
+	ctx.SetCookie(&http.Cookie{
+		Name:     cookieName(ctx),
+		Value:    signedString,            // Set the cookie's value
+		MaxAge:   63072000,                // Max-Age is 2 YEARS (60s * 60min * 24h * 365d * 2y)
+		Path:     "/",                     // This allows the cookie on all paths of this site.
+		Secure:   ctx.IsTLS(),             // Set secure cookies if we're on a secure connection
+		HttpOnly: true,                    // Cookies should only be accessible via HTTPS (not client-side scripts)
+		SameSite: http.SameSiteStrictMode, // Strict same-site policy prevents cookies from being used by other sites.
+		// NOTE: Domain is excluded because it is less restrictive than omitting it. [https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies]
+	})
+
+	// Success!
+	return nil
 }
 
 // cookieName returns the correct cookie name to use, based on the kind of connection.
