a5403e21ddb36bf253331453acbd90a6d79c8202
Ben Pate <ben@pate.org>
2023-08-25 21:19:48 -0600

    Fix BCrypt max length

--- plugin/hash/bcrypt.go
+++ plugin/hash/bcrypt.go
@@ -17,10 +17,14 @@ func (bc BCrypt) ID() string {
 // HashPassword returns a hashed value for the password.
 func (bc BCrypt) HashPassword(plaintext string) (hashedValue string, error error) {
 
+	// Trim password to 72 characters, which is the maximum length that bcrypt will accept.
+	// https://pkg.go.dev/golang.org/x/crypto/bcrypt#GenerateFromPassword
+	plaintext = maxlength(plaintext, 72)
+
 	result, err := bcrypt.GenerateFromPassword([]byte(plaintext), int(bc))
 
 	if err != nil {
-		return "", derp.NewInternalError("steranko.plugin.hash.HashPassword", "Error hashing plaintext", err)
+		return "", derp.NewInternalError("steranko.plugin.hash.HashPassword", "Error hashing plaintext", err.Error())
 	}
 
 	return string(result), nil
@@ -28,6 +32,10 @@ func (bc BCrypt) HashPassword(plaintext string) (hashedValue string, error error
 
 // CompareHashedPassword checks that a hashedValue value matches the plaintext password.
 func (bc BCrypt) CompareHashedPassword(hashedValue string, plaintext string) (OK bool, rehash bool) {
+
+	// Trim password to 72 characters, which is the maximum length that bcrypt will accept.
+	// https://pkg.go.dev/golang.org/x/crypto/bcrypt#GenerateFromPassword
+	plaintext = maxlength(plaintext, 72)
 
 	// Try to validate the password.  If it cannot be matched, then return failure.
 	if err := bcrypt.CompareHashAndPassword([]byte(hashedValue), []byte(plaintext)); err != nil {
@@ -37,15 +45,27 @@ func (bc BCrypt) CompareHashedPassword(hashedValue string, plaintext string) (OK
 	}
 
 	// Try to compute the password cost.
-	if cost, err := bcrypt.Cost([]byte(hashedValue)); cost < int(bc) {
+	cost, err := bcrypt.Cost([]byte(hashedValue))
 
+	if err != nil {
 		// Silently report this error because we don't want to interrupt the application flow.
-		derp.Report(derp.NewInternalError("steranko.plugin.hash.CompareHashedPassword", "Error generating password cost", err))
+		derp.Report(derp.NewInternalError("steranko.plugin.hash.CompareHashedPassword", "Error generating password cost", err.Error()))
+	}
 
+	if cost < int(bc) {
 		// TRUE, TRUE means that the password is OK, but needs to be re-hashed
 		return true, true
 	}
 
 	// TRUE, FALSE means that the password is OK, and doesn't need to be re-hashed
 	return true, false
+}
+
+func maxlength(value string, max int) string {
+
+	if len(value) > max {
+		return value[:max]
+	}
+
+	return value
 }
--- plugin/hash/bcrypt_test.go
+++ plugin/hash/bcrypt_test.go
@@ -3,14 +3,14 @@ package hash
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestBCrypt(t *testing.T) {
 
 	p := BCrypt(10)
 
-	assert.Equal(t, "BCrypt", p.ID())
+	require.Equal(t, "BCrypt", p.ID())
 
 	// https://www.xkcd.com/936/
 	testValues := []string{"trombone", "Tr0ub4dor&3", "correcthorsebatterystaple", "we hold these truths to be self evident, that all men are created equal, and are endowed by their creator with certain inalienable rights.  That among these rights are Life, Liberty, and the Persuit of Happiness."}
@@ -18,30 +18,35 @@ func TestBCrypt(t *testing.T) {
 	for _, plaintext := range testValues {
 
 		hashedValue, err := p.HashPassword(plaintext)
-		assert.NotEqual(t, plaintext, hashedValue)
-		assert.Nil(t, err)
+		require.NotEqual(t, plaintext, hashedValue)
+		require.Nil(t, err)
 
 		// Test incorrect password
 		{
 			ok, update := p.CompareHashedPassword(hashedValue, "not-the-password")
-			assert.False(t, ok)
-			assert.False(t, update)
-			assert.Equal(t, 60, len(hashedValue))
+			require.False(t, ok)
+			require.False(t, update)
+			require.Equal(t, 60, len(hashedValue))
 		}
 
 		// Test matching password
 		{
 			ok, update := p.CompareHashedPassword(hashedValue, plaintext)
-			assert.True(t, ok)
-			assert.False(t, update)
+			require.True(t, ok)
+			require.False(t, update)
 		}
 
 		// Test out-of-date cipher
 		{
 			newCipher := BCrypt(11)
 			ok, update := newCipher.CompareHashedPassword(hashedValue, plaintext)
-			assert.True(t, ok)
-			assert.True(t, update)
+			require.True(t, ok)
+			require.True(t, update)
 		}
 	}
+}
+
+func TestMaxlength(t *testing.T) {
+	require.Equal(t, "hello", maxlength("hello", 10))
+	require.Equal(t, "hell", maxlength("hello", 4))
 }
